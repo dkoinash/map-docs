47374a639678205763a299f2496ea647a350ae45
Denis Koinash <d.koinash@cyclone-nov.ru>
2022-12-23 17:49:35 +0300

    class Token added

--- include/structures.h
+++ include/structures.h
@@ -1,10 +1,14 @@
 #pragma once
 
-#include <cstdint>
 #include <array>
+#include <cstdint>
+#include <cstring>
+#include <cmath>
+#include <string>
+
 /**
  * @brief https://github.com/sk-zk/map-docs/wiki/Item-type-IDs
- * 
+ *
  */
 enum class ItemTypeID : uint8_t
 {
@@ -45,24 +49,86 @@ enum class ItemTypeID : uint8_t
 };
 /**
  * @brief https://github.com/sk-zk/map-docs/wiki/kDOP-bounding-box
- * 
+ *
  */
-struct KDOP{
+struct KDOP
+{
   std::array<float, 5> minimums;
   std::array<float, 5> maximums;
 };
 /**
  * @brief https://github.com/sk-zk/map-docs/wiki/kDOP-item
- * 
+ *
  */
-struct KDOP_ITEM{
+struct KDOP_ITEM
+{
   ItemTypeID item_type;
   /// The UID of the item.
   uint64_t uid;
   /// kDOP bounding box
   KDOP kdop;
-  /// Despite the semantics, this is the main flag field for every item type, with each using different, item-specific flags.
+  /// Despite the semantics, this is the main flag field for every item type, with each using
+  /// different, item-specific flags.
   uint32_t flags;
-  /// View distance of the item in meters, divided by 10. Default is 400m. The maximum the game will let you use is 1500m.
+  /// View distance of the item in meters, divided by 10. Default is 400m. The maximum the game will
+  /// let you use is 1500m.
   uint8_t view_list;
+};
+
+
+class Token
+{
+public:
+  static const int MaxLength     = 12;
+  static const int CharsetLength = 38;
+
+  bool IsValidToken(std::string str){
+    if(str.length() > MaxLength) return false;
+    for (auto c : str) {
+      bool check = false;
+      for (int i = 0; i < CharsetLength; ++i) {
+        if (c == CharacterSet[i]) {
+          check = true;
+          break;
+        }
+      }
+      if (check == false) return false;
+    }
+    return true;
+  }
+  
+  int GetCharIndex(char letter){
+    int index = -1;
+    for(int i = 0; i < CharsetLength; ++i){
+      if(letter == CharacterSet[i]){
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+
+  uint64_t StringToToken(std::string input){
+    if(input.empty()) return 0;
+    if (!IsValidToken(input))
+                throw ("Input is not a valid token.");
+    for(auto c:input){
+      std::tolower(c);
+    }
+    uint64_t token;
+    for(int i = 0; i < input.length(); ++i){
+      token += (uint64_t)std::pow(CharsetLength, i) * (uint64_t)GetCharIndex(input[i]);
+    }
+    return token;
+  }
+
+  Token(uint64_t token) { Value = token; }
+  
+  Token(std::string str) { Value = StringToToken(str); }
+
+private:
+  uint64_t Value;
+  char     CharacterSet[38] = { '\0', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+                            'c',  'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                            'p',  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_' };
 };
